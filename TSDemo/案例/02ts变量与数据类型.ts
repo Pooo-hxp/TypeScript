09324b5c1785e1b33c5d31bb90ff587b876cfaa5
GitHpotato <hengxipeng@qq.com>
2020-07-14 18:00:36 +0800

    ts中变量要进行类型声明

---
+++ TSDemo/案例/02ts变量与数据类型.ts
@@ -0,0 +1,11 @@
+/**
+ * 变量要进行数据类型声明
+ * 并且类型要对应，否则编译器就会报错
+ */
+let Dnum:number=2020;
+var Dname:string='土豆';
+var Dname2:boolean=false;
+   Dnum=444//
+   //Dnum='444' 报错：无法将类型’444‘分配给类型number
+let undef:undefined=undefined;   
+let nul:null=null;   
