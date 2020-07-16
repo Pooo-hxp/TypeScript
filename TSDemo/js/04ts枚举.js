6a04a212b647c1121b88a96833df363af34374e5
GitHpotato <hengxipeng@qq.com>
2020-07-16 20:54:50 +0800

    添加TS枚举

---
+++ TSDemo/js/04ts枚举.js
@@ -0,0 +1,10 @@
+"use strict";
+/**
+ * 枚举
+ */
+var langtype;
+(function (langtype) {
+    langtype[langtype["javascript"] = 1] = "javascript";
+    langtype[langtype["typescript"] = 2] = "typescript";
+    langtype[langtype["es6"] = 3] = "es6";
+})(langtype || (langtype = {}));
---
+++ TSDemo/案例/04ts枚举.ts
@@ -0,0 +1,8 @@
+/**
+ * 枚举
+ */
+enum langtype {
+    javascript = 1,
+    typescript = 2,
+    es6 = 3
+}
