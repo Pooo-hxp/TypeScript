13255a860889197212806f69d593b999b361db65
GitHpotato <hengxipeng@qq.com>
2020-07-20 22:38:23 +0800

    添加形参和默认值设定

---
+++ TSDemo/js/09ts中的函数可选参数.js
@@ -0,0 +1,22 @@
+"use strict";
+/**
+ * 可选参数：就是在形参后边添加问号‘？’
+ * function 函数名(形参?:类型):返回值类型{
+ *  }
+ * 此时这里的实参可传可不传
+ */
+function anyPar(city) {
+    console.log("\u6211\u60F3\u53BB" + city);
+}
+anyPar('郑州');
+anyPar();
+/**
+ * 使用默认参数
+ * function 函数名(形参1:类型,形参2:类型=默认值2):返回值类型{
+ *  }
+ */
+function anyPar2(city, doSom) {
+    if (doSom === void 0) { doSom = '写代码'; }
+    console.log("\u6211\u60F3\u53BB" + city + "\u7136\u540E" + doSom);
+}
+anyPar2('杭州');
---
+++ TSDemo/案例/09ts中的函数可选参数.ts
@@ -0,0 +1,20 @@
+/**
+ * 可选参数：就是在形参后边添加问号‘？’
+ * function 函数名(形参?:类型):返回值类型{
+ *  }
+ * 此时这里的实参可传可不传
+ */
+function anyPar(city?:string):void{
+    console.log(`我想去${city}`); 
+}
+anyPar('郑州')
+anyPar()
+/**
+ * 使用默认参数
+ * function 函数名(形参1:类型,形参2:类型=默认值2):返回值类型{
+ *  }
+ */
+function anyPar2(city:string,doSom:string='写代码'):void{
+    console.log(`我想去${city}然后${doSom}`);
+}
+anyPar2('杭州')
