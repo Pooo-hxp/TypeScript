4532c5b4158969264bb58ce4426a638578384c78
GitHpotato <hengxipeng@qq.com>
2020-08-06 00:58:31 +0800

    添加Vue模板

--- TSDemo/案例/a4ts-vue_ts脚手架/poo-demo/src/App.vue
+++ TSDemo/案例/a4ts-vue_ts脚手架/poo-demo/src/App.vue
@@ -7,14 +7,19 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+@Component
+export default class App extends Vue{
+  
+}
+
+// @Component({
+//   components: {
+//     HelloWorld,
+//   },
+// })
+// export default class App extends Vue {}
 </script>
 
 <style lang="stylus">
