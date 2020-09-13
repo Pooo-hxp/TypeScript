902ad6ddd3664cff29ea2fdf011c7f656de0fa32
poo <hengxipeng@qq.com>
2020-09-13 22:34:38 +0800

    补充实例

---
+++ Vue3.0/PoooName/src/App_test_ref.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{tip}}</p>
+  </div>
+</template>
+
+<script>
+import { removeItem } from "./remove"; //导入删除的业务逻辑模块
+export default {
+  name: "App",
+  setup() {
+    let tip=reactive({
+      tip:'Hi～'
+    })
+    return { stus, removeVeget };
+  },
+  methods: {},
+};
+</script>
+<style lang="">
+* {
+  list-style: none;
+}
+</style>
