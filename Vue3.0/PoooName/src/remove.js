a3942a63696b92e2c23ee50fdf7a373f91417c6d
GitHpotato <hengxipeng@qq.com>
2020-09-23 22:38:34 +0800

    把删除业务模块分离

---
+++ Vue3.0/PoooName/src/remove.js
@@ -0,0 +1,15 @@
+import { reactive } from "vue"; //引入依赖
+function removeItem() {
+    let stus = reactive({
+      stusList: [
+        { id: 1, Name: "potato", price: "2.5" },
+        { id: 2, Name: "tomato", price: "3.5" },
+        { id: 3, Name: "cucumber", price: "4.5" },
+      ],
+    });
+    function removeVeget(index) {
+      stus.stusList.splice(index, 1);
+    }
+    return {stus,removeVeget} 
+  }
+  export default removeItem;
