34a7fd31a69e27ede6444eb768d464938634c249
poo <hengxipeng@qq.com>
2020-09-30 22:03:36 +0800

    补充测试文件

--- Vue3.0/PoooName/src/App_Proxy.vue
+++ Vue3.0/PoooName/src/App_Proxy.vue
@@ -1,33 +1,44 @@
 <template>
   <div>
-    <p>{{testJson.tip}}</p>
-    <p>{{testArray[2]}}</p>
-    <p>{{testString}}</p>
-    <button @click="showProxyPar">showSomething </button>
+    <p>{{only.name}}</p>
+    <p>{{only.age}}</p>
+    <button @click="myFun" ref="btn">clickMe</button>
   </div>
 </template>
 
 <script>
-import {reactive} from 'vue';
+import {readonly} from "vue";
+
 export default {
   name: "App",
   setup() {
-    let testJson=reactive({
-      tip:'its a Json！'
-    })
-    let testArray=reactive(['first','second','third'])
-    let testString=reactive('Just a string')
-    function showProxyPar(){
-      testJson.tip='changed';
-      testArray[2]='selected';
-      testString='hengxipeng';
-      console.log(testJson);// Proxy {tip: "changed"}
-      console.log(testArray);// {0: "first", 1: "second", 2: "selected"}
-      console.log(testString);// hengxipeng
+    let obj={
+      name:'poo',
+      age:'13'
     }
-    return { testJson,testArray,testString,showProxyPar };
+    let objProxy=new Proxy(obj,{
+      //数据读会触发
+      get(obj,key){
+        console.log(obj);//{name: "poo", age: "13"}
+        return obj[key]
+      },
+      //监听的数据被修改会触发
+      set(obj,key,value){
+      // 操作的对象，操作的属性，赋予的新值
+      obj[key]=value //把外界赋予的新值更新到该对象
+      console.log('进行UI之类的操作');
+      }
+    })
+   objProxy.name;
+    let only=readonly(obj)
+
+
+    function myFun() {
+      only.name='HAHA';//  failed: target is readonly
+    }
+    return {only, myFun };
   },
-  methods: {},
+
 };
 </script>
 <style lang="">
---
+++ Vue3.0/PoooName/src/App_reactive.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>{{testJson.tip}}</p>
+    <p>{{testArray[2]}}</p>
+    <p>{{testString}}</p>
+    <button @click="showProxyPar">showSomething </button>
+  </div>
+</template>
+
+<script>
+import {reactive} from 'vue';
+export default {
+  name: "App",
+  setup() {
+    let testJson=reactive({
+      tip:'its a Json！'
+    })
+    let testArray=reactive(['first','second','third'])
+    let testString=reactive('Just a string')
+    function showProxyPar(){
+      testJson.tip='changed';
+      testArray[2]='selected';
+      testString='hengxipeng';
+      console.log(testJson);// Proxy {tip: "changed"}
+      console.log(testArray);// {0: "first", 1: "second", 2: "selected"}
+      console.log(testString);// hengxipeng
+    }
+    return { testJson,testArray,testString,showProxyPar };
+  },
+  methods: {},
+};
+</script>
+<style lang="">
+* {
+  list-style: none;
+}
+</style>
