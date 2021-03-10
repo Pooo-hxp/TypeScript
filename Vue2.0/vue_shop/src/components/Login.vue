deb9417565933244b57c4fa73ddc149913bd18d6
Pooo-hxp <hengxipeng@qq.com>
2021-03-10 21:48:07 +0800

    axios获取登录状态

--- Vue2.0/vue_shop/src/components/Login.vue
+++ Vue2.0/vue_shop/src/components/Login.vue
@@ -6,19 +6,31 @@
         <img src="@/assets/logo.png" alt="" />
       </div>
       <!-- 表单区 -->
-      <el-form class="login_info" :model="loginForm">
-        <el-form-item label="">
+      <el-form
+        ref="loginFormRef"
+        class="login_info"
+        :rules="loginFormRules"
+        :model="loginForm"
+      >
+        <el-form-item label="" prop="username">
           <!-- 用户名 -->
-          <el-input  prefix-icon="iconfont icon-user" v-model="loginForm.username"></el-input>
+          <el-input
+            prefix-icon="iconfont icon-user"
+            v-model="loginForm.username"
+          ></el-input>
         </el-form-item>
-        <el-form-item label="">
+        <el-form-item label="" prop="password">
           <!-- 密码 -->
-          <el-input  prefix-icon="iconfont icon-3702mima" v-model="loginForm.password"></el-input>
+          <el-input
+            prefix-icon="iconfont icon-3702mima"
+            v-model="loginForm.password"
+            type="password"
+          ></el-input>
         </el-form-item>
         <el-form-item class="btns">
           <!-- 登录按钮 -->
-           <el-button type="success" round>登录</el-button>
-           <el-button type="info" round>重置</el-button>
+          <el-button type="success" round @click="login">登录</el-button>
+          <el-button type="info" round @click="reSet">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -31,11 +43,47 @@ export default {
   data() {
     return {
       //登录表单数据对象
-      loginForm:{
-        username:'name',
-        password:'123',
-      }
+      loginForm: {
+        username: "name",
+        password: "123",
+      },
+      //表单校验设置
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入账号密码", trigger: "change" },
+          {
+            min: 6,
+            max: 13,
+            message: "密码长度在 6 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
     };
+  },
+  methods: {
+    //将表单重置为默认值且移除校验规则
+    reSet() {
+      this.$refs.loginFormRef.resetFields();
+    },
+    login() {
+      /**
+       * 表单提交前预验证
+       * val 格式校验返回布尔值
+       * true 数据通过验证
+       * false 数据格式校验失败
+       */
+      this.$refs.loginFormRef.validate(async val => {
+        if(!val) return ;
+        // 解构获取返回参数data，赋值给res
+        const {data:res}=await this.$http.post('login',this.loginForm);
+        return res.meta.status!==200?console.log('登录失败'):console.log('登录成功');
+      });
+    },
   },
 };
 </script>
@@ -74,14 +122,14 @@ export default {
     }
   }
 }
-.login_info{
+.login_info {
   position: absolute;
   width: 100%;
   padding: 0 20px;
   box-sizing: border-box;
   bottom: 0;
 }
-.btns{
+.btns {
   display: flex;
   justify-content: flex-end;
 }
--- Vue2.0/vue_shop/src/main.js
+++ Vue2.0/vue_shop/src/main.js
@@ -1,18 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 //全局样式表
-import '@/assets/css/global.css'
+import '@/assets/css/global.css';
 // 三方资源，阿里矢量库
-import '@/assets/fonts/iconfont.css'
-
+import '@/assets/fonts/iconfont.css';
+//引用axios用于交互
+import axios from 'axios';
+// 配置请求默认地址
+axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/';
+Vue.prototype.$http=axios;
 // elementUI 资源
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(Element)
-Vue.config.productionTip = false
+Vue.use(Element);
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
