cfea1fe087bcb2b23d023581a922fdbb30fbe672
Pooo-hxp <hengxipeng@qq.com>
2021-03-11 22:01:18 +0800

    设定布局

--- Vue2.0/vue_shop/src/components/Home.vue
+++ Vue2.0/vue_shop/src/components/Home.vue
@@ -1,8 +1,19 @@
 <!--  -->
 <template>
-  <div>
-    <el-button type="warning" @click="out">退出</el-button>
-  </div>
+    <el-container class="home-style">
+      <!-- 头部 -->
+      <el-header>
+        Header
+        <el-button type="warning" @click="out">退出</el-button>
+      </el-header >
+      <!-- 主体 -->
+      <el-container>
+        <!-- 左侧边 -->
+        <el-aside width="200px">Aside</el-aside>
+        <!-- 右主体 -->
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
 </template>
 
 <script>
@@ -20,4 +31,16 @@ export default {
 };
 </script>
 <style lang='less' scoped>
+.home-style{
+  height: 100%;
+}
+.el-header{
+  background-color: #363D40;
+}
+.el-aside{
+  background-color: #313743;
+}
+.el-main{
+  background-color: #E9EDF1;
+}
 </style>
