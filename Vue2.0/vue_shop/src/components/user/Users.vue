979a3fa589c265942504abe77b3c5fee7a52e1a7
Pooo-hxp <hengxipeng@qq.com>
2021-03-13 21:51:38 +0800

    获取用户数据

--- Vue2.0/vue_shop/src/components/user/Users.vue
+++ Vue2.0/vue_shop/src/components/user/Users.vue
@@ -18,7 +18,7 @@
             ></el-button> </el-input
         ></el-col>
         <el-col :span="4">
-            <el-button type="primary">添加用户</el-button>
+          <el-button type="primary">添加用户</el-button>
         </el-col>
       </el-row>
     </el-card>
@@ -29,7 +29,31 @@
 export default {
   name: "users",
   data() {
-    return {};
+    return {
+      userList:[],
+      quertInfo: {
+        query: "",
+        pagenum: 1,
+        pagesize: 2,
+      },
+      total:0
+    };
+  },
+  methods: {
+    async getUserList() {
+      console.log('---');
+      const { data: res } = await this.$http.get("users", {
+        params: this.quertInfo,
+      });
+      if(res.meta.status!==200){ return this.$message.errto('获取用户列表数据失败')}
+      this.userList=res.data.users;
+      this.total=res.data.total;
+      console.log(this.total);
+      console.log(this.userList);
+    },
+  },
+  created() {
+    this.getUserList();
   },
 };
 </script>
