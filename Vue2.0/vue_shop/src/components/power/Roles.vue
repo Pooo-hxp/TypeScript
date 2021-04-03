1406f3fbd1a21f9e31cc0068a107a7f7da0a098f
Pooo-hxp <hengxipeng@qq.com>
2021-04-03 13:49:31 +0800

    格式化权限

--- Vue2.0/vue_shop/src/components/power/Roles.vue
+++ Vue2.0/vue_shop/src/components/power/Roles.vue
@@ -8,7 +8,7 @@
       <el-breadcrumb-item>角色列表</el-breadcrumb-item>
     </el-breadcrumb>
     <!-- 卡片视图 -->
-    <el-card >
+    <el-card>
       <!-- 添加角色按钮 -->
       <el-row>
         <el-col>
@@ -16,18 +16,42 @@
         </el-col>
       </el-row>
       <!-- 角色列表区域 -->
-      <el-table :data='roleList' border stripe>
+      <el-table :data="roleList" border stripe>
+        <!-- 展开列 -->
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+            <el-row :class="['bd-bottom',i1==0?'bd-top':'']" v-for="(item1, i1) in scope.row.children" :key="item1.id">
+              <!-- 一级权限 -->
+              <el-col :span="5">
+                <el-tag>
+                  {{ item1.authName }}
+                </el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <!-- 二级和三级 -->
+              <el-col :span="19"></el-col>
+            </el-row>
+            <pre>
+            {{ scope.row }}
+            </pre>
+          </template>
+        </el-table-column>
+        <!-- 索引序号列 -->
         <el-table-column type="index"></el-table-column>
-        <el-table-column label="角色名称" prop="roleName">
-        </el-table-column>
-        <el-table-column label="角色描述" prop="roleDesc">
-        </el-table-column>
-         <el-table-column label="操作" width="300px" >
-            <template slot-scope="scope">
-              <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
-              <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
-              <el-button size="mini" type="warning" icon="el-icon-setting">分配权限</el-button>
-            </template>
+        <el-table-column label="角色名称" prop="roleName"> </el-table-column>
+        <el-table-column label="角色描述" prop="roleDesc"> </el-table-column>
+        <el-table-column label="操作" width="300px">
+          <template slot-scope="scope">
+            <el-button size="mini" type="primary" icon="el-icon-edit"
+              >编辑</el-button
+            >
+            <el-button size="mini" type="danger" icon="el-icon-delete"
+              >删除</el-button
+            >
+            <el-button size="mini" type="warning" icon="el-icon-setting"
+              >分配权限</el-button
+            >
+          </template>
         </el-table-column>
       </el-table>
     </el-card>
@@ -40,24 +64,33 @@ export default {
   data() {
     return {
       // 角色列表数据
-      roleList:[]
+      roleList: [],
     };
   },
-  created(){
+  created() {
     this.getRolesList();
   },
-  methods:{
+  methods: {
     // 获取角色列表
-    async getRolesList(){
-      const{data:res}=await this.$http.get('roles')
-      if(res.meta.status!==200){
-        return this.$message.error('获取角色列表失败！')
+    async getRolesList() {
+      const { data: res } = await this.$http.get("roles");
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取角色列表失败！");
       }
-      this.roleList=res.data;
+      this.roleList = res.data;
       console.log(this.roleList);
-    }
-  }
+    },
+  },
 };
 </script>
 <style lang='less' scoped>
+.el-tag {
+  margin: 7px;
+}
+.bd-top{
+  border-top: 1px solid #eee;
+}
+.bd-bottom{
+  border-bottom: 1px solid #eee;
+}
 </style>
