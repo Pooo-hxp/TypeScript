70f35ce9b3519c66175ca4793fc49036f2b8bc97
Pooo-hxp <hengxipeng@qq.com>
2021-04-18 22:34:59 +0800

    配置商品分类组件

---
+++ Vue2.0/vue_shop/src/components/goods/Cate.vue
@@ -0,0 +1,26 @@
+<!--  -->
+<template>
+  <div>
+      商品分配组件
+  </div>
+</template>
+
+<script>
+export default {
+name:'',
+  data () {
+    return {
+    };
+  },
+  created(){
+
+  },
+  methods(){
+
+  }
+
+}
+
+</script>
+<style lang='' scoped>
+</style>
