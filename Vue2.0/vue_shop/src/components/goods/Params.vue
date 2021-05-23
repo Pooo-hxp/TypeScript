b1f0bd19ba4169d809b27f84c2852e41956b6204
Pooo-hxp <hengxipeng@qq.com>
2021-05-23 19:07:51 +0800

    依据面板变动获取相关参数

--- Vue2.0/vue_shop/src/components/goods/Params.vue
+++ Vue2.0/vue_shop/src/components/goods/Params.vue
@@ -8,26 +8,41 @@
     </el-breadcrumb>
     <!-- 卡片视图区域 -->
     <el-card>
-        <!-- 头部警告 -->
-         <el-alert title="注意：只允许为第三级分类设置相关参数" type="warning" :closable="false" show-icon> </el-alert>
-        <!-- 商品选择区域 -->
-        <el-row class="cat_opt">
-            <el-col>
-                <span>选择商品分类：</span>
-                <!-- 商品分类级联 -->
-                <el-cascader
-                    v-model="selectCateKeys"
-                    :options="catelist"
-                    :props="cateProps"
-                    @change="handleChange">
-                    </el-cascader>
-            </el-col>
-        </el-row>
-        <!-- 页签区域 -->
-        <el-tabs v-model="activeName" @tab-click="handleTableClick">
-        <el-tab-pane label="动态参数" name="first">用户管理</el-tab-pane>
-        <el-tab-pane label="静态属性" name="second">配置管理</el-tab-pane>
-    </el-tabs>
+      <!-- 头部警告 -->
+      <el-alert
+        title="注意：只允许为第三级分类设置相关参数"
+        type="warning"
+        :closable="false"
+        show-icon
+      >
+      </el-alert>
+      <!-- 商品选择区域 -->
+      <el-row class="cat_opt">
+        <el-col>
+          <span>选择商品分类：</span>
+          <!-- 商品分类级联 -->
+          <el-cascader
+            v-model="selectCateKeys"
+            :options="catelist"
+            :props="cateProps"
+            @change="handleChange"
+          >
+          </el-cascader>
+        </el-col>
+      </el-row>
+      <!-- 页签区域 -->
+      <el-tabs v-model="activeName" @tab-click="handleTableClick">
+        <!-- 动态参数面板 -->
+        <el-tab-pane label="动态参数" name="many">
+          <el-button type="primary" size="mini" :disabled="isBtnDis"
+            >添加参数</el-button
+          >
+        </el-tab-pane>
+        <!-- 静态属性面板 -->
+        <el-tab-pane label="静态属性" name="only" :disabled="isBtnDis">
+          <el-button type="primary" size="mini">添加属性</el-button>
+        </el-tab-pane>
+      </el-tabs>
     </el-card>
   </div>
 </template>
@@ -36,49 +51,72 @@
 export default {
   data() {
     return {
-        //商品分类列表
-        catelist:[],
-        //为级联选择配置对象
-        cateProps:{
-            value:'cat_id',
-            label:'cat_name',
-            children:'children'
-        },
-        // 级联选择框选中的数据
-        selectCateKeys:[],
-        // 激活页签名称
-        activeName:'first'
+      //商品分类列表
+      catelist: [],
+      //为级联选择配置对象
+      cateProps: {
+        value: "cat_id",
+        label: "cat_name",
+        children: "children",
+      },
+      // 级联选择框选中的数据
+      selectCateKeys: [],
+      // 激活页签名称
+      activeName: "many",
     };
   },
   created() {
-      this.getCateList()
+    this.getCateList();
   },
   methods: {
-      //获取商品分配数据
-      async getCateList() {
-          const {data:res}=await this.$http.get('cateGories')
-          if(res.meta.status!==200){
-              return this.$message.error('获取商品分类失败！')
-          }
-          this.catelist=res.data;
-      },
-      //级联选择框数据变化出发
-      handleChange(){
-          //选中的商品分三级，则清空选中
-          if(this.selectCateKeys.length!==3){
-              this.selectCateKeys.length=0;
-          }
-      },
-      //tab页签点击事件处理函数
-      handleTableClick(){
-          console.log(this.activeName);
+    //获取商品分配数据
+    async getCateList() {
+      const { data: res } = await this.$http.get("cateGories");
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取商品分类失败！");
       }
+      this.catelist = res.data;
+    },
+    //级联选择框数据变化出发
+    handleChange() {
+        this.getParamsData();
+    },
+    //tab页签点击事件处理函数
+    handleTableClick() {
+        this.getParamsData();
+    },
+    async getParamsData(){
+              //选中的商品分三级，则清空选中
+      if (this.selectCateKeys.length !== 3) {
+        this.selectCateKeys.length = 0;
+      }
+      //依据所处面板及分类ID，获取对应参数
+      const {data:res}= await this.$http.get(`categories/${this.cateId}/attributes`, {
+        params: { sel: this.activeName },
+      });
+      if(res.meta.status!==200){
+          return this.$message.error('获取参数列表失败！')
+      }
+      console.log(res.data);
+    }
+    
+  },
+  computed: {
+    //按钮被禁用
+    isBtnDis() {
+      // 选中商品非三类，则禁用控制面板
+      return this.selectCateKeys.length !== 3 ? true : false;
+    },
+    // 当前选中的三级分类ID
+    cateId() {
+      return this.selectCateKeys.length === 3 ? this.selectCateKeys[2] : null;
+    },
   },
 };
 </script>
 
 <style lang="less" scoped>
-.cat_opt{
-    margin: 15px 0;
+.cat_opt {
+  margin: 15px 0;
 }
 </style>
