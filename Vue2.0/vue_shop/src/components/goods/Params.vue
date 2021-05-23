9421412df9860c5591bf93c39873f20d79d6f43e
Pooo-hxp <hengxipeng@qq.com>
2021-05-23 17:42:28 +0800

    配置参数组件路由及面包屑

--- Vue2.0/vue_shop/src/components/goods/Params.vue
+++ Vue2.0/vue_shop/src/components/goods/Params.vue
@@ -1,19 +1,28 @@
 <template>
   <div>
-      分类参数
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">商品管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>参数列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+        <!-- 头部警告 -->
+      <el-alert title="注意：只允许为第三级分类设置相关参数" type="warning" :closable="false" show-icon> </el-alert>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{}
-    },
-    created(){},
-    methods(){},
-}
+  data() {
+    return {};
+  },
+  created() {},
+  methods() {},
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
