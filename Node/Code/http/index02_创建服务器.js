a17df0533da555804776aeeec3eaa2825995a353
Pooo-hxp <hengxipeng@qq.com>
2021-09-20 17:18:43 +0800

    feat(node服务器的创建): 补充两种方式的差异

---
+++ Node/Code/http/index02_创建服务器.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const Server1 = http.createServer((req, res)=>{
+    res.end("Server1");
+})
+Server1.listen(8001,()=>{
+    console.log('Server1 启动~~');
+})
+
+const Server2 = http.createServer((req, res)=>{
+    res.end("Server2");
+})
+Server2.listen(8002,()=>{
+    console.log('Server2 启动~~');
+})
+
+// 本质上这种方法和上方是一致的
+const Server3 = new http.Server((req, res)=>{
+    res.end("Server3");
+})
+Server3.listen(8003,()=>{
+    console.log('Server3 启动~~');
+})
